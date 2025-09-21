6dc409a12cb2bc5f85a0ef5c55bab0a855dbd8a0
hafizuddin <41276836+hafizuddin@users.noreply.github.com>
2025-09-22 03:32:54 +0800

    Used for loop to reverse the string & .reverse() I learnt/reminded that I can use string[i] to access each of letter within the string .reverse() is array method but can also be use if you convert the string to method

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+// const reverseString = function(string) {
+//     let reversed = "";
+//     for (let i = string.length -1; i >= 0; i--){
+//         reversed += string[i];
+//     }
+//     return reversed;
+// };
 
-};
+// or using .reverse method but need to remember this method is for array
+// so "convert" string to array using split then join them back 
+
+// const reverseString = function(string) {
+//     return string.split("").reverse().join("");
+// };
+
+
+const reverseString = string => string.split("").reverse().join("");
 
 // Do not edit below this line
 module.exports = reverseString;
