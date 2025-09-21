3aaf2f6a1627d3c70ef2355713a3bad62d1f04cb
hafizuddin <41276836+hafizuddin@users.noreply.github.com>
2025-09-22 05:03:06 +0800

    remember if (min > max) [min, max] = [max, min];

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
 
+    if (!Number.isInteger(first) || !Number.isInteger(last)) return "ERROR";
+
+    if (last < 0 || first < 0) return "ERROR";
+
+    if (first>last) {
+        let temp = first;
+        first = last;
+        last = temp;
+
+    }
+
+    // An alternative way to swap the values of min and max like above is to use the array destructuring syntax.
+    // Here's an optional article on it: https://www.freecodecamp.org/news/array-destructuring-in-es6-30e398f21d10/
+    // if (min > max) [min, max] = [max, min];
+
+    let sum = 0;
+    for (i = first; i<=last; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
